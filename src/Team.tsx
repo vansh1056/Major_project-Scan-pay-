db78b12d5fee2a94364d8a194934eaf2eae0f13b
Vansh1056 <vanshofficial26h6@gmail.com>
2025-03-21 08:04:08 +0530

    Add images

--- src/Team.tsx
+++ src/Team.tsx
@@ -5,23 +5,23 @@ const teamMembers = [
   {
     name: "Vansh Singh",
     role: "Frontend Developer",
-    image: "/Images/member1.jpg",
+    image: "/images/member1.jpg",
     // bio:" ahfjsbnfkugjsdkfnsiukfjasifugyvashfnsdufyhsaWVkf",
   },
   {
     name: "Shiv Panwar",
     role: "Backend Developer",
-    image: "/Images/member2.jpeg",
+    image: "/images/member2.jpeg",
   },
   {
     name: "Sharandeep Meharwal",
     role: "Database Manager",
-    image: "/Images/member3.jpeg",
+    image: "/images/member3.jpeg",
   },
   {
     name: "Yaduvendra Choudhary",
     role: "Documentation Specialist",
-    image: "/Images/member4.jpeg",
+    image: "/images/member4.jpeg",
   },
 ];
 
